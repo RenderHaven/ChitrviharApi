87cd9c5dcaf884f4c275aab7c1ad430242f719ee
RenderHaven <vikrambalai1002@gmail.com>
2025-01-08 19:10:35 +0530

    hii

--- app.py
+++ app.py
@@ -3,20 +3,23 @@ from flask_cors import CORS
 from models import db
 from product import product_bp  # Import the Blueprint
 from iteam import iteam_bp  # Import the Blueprint
+import os
 
 app = Flask(__name__)
 
-CORS(app)
-# Configure SQLite connection string
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Configure the database connection string from environment variables, fallback to SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-# Register the Blueprint
+# Register Blueprints
 app.register_blueprint(product_bp, url_prefix='/product')  # Prefixing all product routes with /product
-app.register_blueprint(iteam_bp, url_prefix='/item')  # Prefixing all product routes with /product
+app.register_blueprint(iteam_bp, url_prefix='/item')  # Prefixing all item routes with /item
 
+# Ensure database tables are created
 with app.app_context():
     db.create_all()
 
@@ -25,4 +28,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
